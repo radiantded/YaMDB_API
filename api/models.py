263f1a137bcc9a43781a5b7b98bbd410a7a64e24
melkomukovki <Melkomukovki@nspk.ru>
2021-06-14 22:50:40 +0300

    fix

--- api/models.py
+++ api/models.py
@@ -12,7 +12,6 @@ def year_validator(value):
 
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Категория', max_length=100)
     slug = models.SlugField(verbose_name='Slug', max_length=50, unique=True)
 
@@ -21,7 +20,6 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Жанр', max_length=100)
     slug = models.SlugField(verbose_name='Slug', max_length=50, unique=True)
 
@@ -30,7 +28,6 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Название', max_length=200)
     year = models.IntegerField(verbose_name='Год выхода',
                                validators=[year_validator])
@@ -46,7 +43,6 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, verbose_name='Автор отзыва',
                              on_delete=models.CASCADE,
                              related_name='reviewer')
@@ -74,7 +70,6 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
-    id = models.AutoField(primary_key=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments'
     )
