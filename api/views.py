1b62b81f185162625f768584218ff3efc4de0f28
Пичугина Валерия <Psyde26@yandex.ru>
2021-06-22 14:29:18 +0300

    flake8

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import mixins, status, views, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (
